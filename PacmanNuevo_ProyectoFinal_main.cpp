543fd6f612d28b84b4fe8024e04e924dcd7b2201
LaloDavilaC9 <36578173+LaloDavilaC9@users.noreply.github.com>
2020-11-24 18:16:22 -0600

    Update PacmanNuevo_ProyectoFinal_main.cpp

--- PacmanNuevo_ProyectoFinal_main.cpp
+++ PacmanNuevo_ProyectoFinal_main.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){
+<<<<<<< HEAD
 	cout<<"Wishin"<<endl;
 	cout<<"Hola"<<endl;
 	cout<<"Hola"<<endl;
 	cout<<"Hola"<<endl;
 	cout<<"Hola"<<endl;
+=======
+	
+>>>>>>> parent of 949a152... Update PacmanNuevo_ProyectoFinal_main.cpp
 }

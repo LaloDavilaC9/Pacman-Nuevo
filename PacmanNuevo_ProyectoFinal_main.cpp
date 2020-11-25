8da603543cbd35e86fb7ebd6d31700ec740863fc
LaloDavilaC9 <36578173+LaloDavilaC9@users.noreply.github.com>
2020-11-24 18:10:44 -0600

    Calando

--- PacmanNuevo_ProyectoFinal_main.cpp
+++ PacmanNuevo_ProyectoFinal_main.cpp
@@ -3,12 +3,9 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
 	cout<<"Wishin"<<endl;
-=======
 	cout<<"Hola"<<endl;
 	cout<<"Hola"<<endl;
 	cout<<"Hola"<<endl;
 	cout<<"Hola"<<endl;
->>>>>>> parent of 313762b... Update PacmanNuevo_ProyectoFinal_main.cpp
 }

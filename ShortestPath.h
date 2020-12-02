905754095f4e105eca54c9570bdc4f7fab512ff4
Brayan-ux <Bsanchez6941@gmail.com>
2020-12-02 11:17:07 -0600

    Update ShortestPath.h

--- ShortestPath.h
+++ ShortestPath.h
@@ -1,20 +1,10 @@
 #pragma once
 
 #include <vector>
-class A_Search {
-	private:
-		struct Nodo {
-			bool obstaculo = false;
-			bool nVisitado = false;
-			float fMetaGlobal;
-			float FMetaLocal;
-			int x, y;
-			std::vector <Nodo *> vecN_Vecinos;
-			Nodo* padre;
-		};
-		Nodo *nodos=nullptr;
-		int anchoMapa=30;
-		int alturaMapa=20;
-	protected:
-		virtual bool creacionDeUsuario() {return true;}
+
+class Nodo {
+	int posX, posY;
+    public:
+        void iniciar(Nodo, Nodo);
+        int *localizar(Nodo);
 };
